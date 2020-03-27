9c83c2417e912d04907839224b21e8922ffcd6e3
Svetoslav Enchev <svetoslav.enchev@gmail.com>
2020-03-27 09:46:13 +0200

    Guessing Game: initial commit

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+use std::io::Read;
+
+fn getch() {
+    let mut buf = [0; 1];
+    io::stdin().read_exact(&mut buf).unwrap();
+}
+
+fn main() {
+    println!("Guessing game!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        println!("Enter your guess:");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        if guess.starts_with("q") {
+            return;
+        }
+
+        let guess = match guess.trim().parse::<u32>() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You guessed {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+    getch();
+}
